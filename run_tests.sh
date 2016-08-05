13f1aac6e6f927c39167af66b4a9d634fafc0df4
shari <shari.matzner@pnnl.gov>
2016-08-05 13:50:02 -0700

    created run_tests.sh in floodMap, rubble and rubblepy

---
+++ run_tests.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+export ROOT_DIR=`pwd`
+export SCRIPT_DIR="${ROOT_DIR}/src"
+export DATA_DIR="${ROOT_DIR}/data"
+export OUTPUT_ROOT="${ROOT_DIR}/output"
+
+# Shari:  add commands to run the radar flood mapping Ike imagery
+
+# See run_tests.sh in ../tornadoTrack
